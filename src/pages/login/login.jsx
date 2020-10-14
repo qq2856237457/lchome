8274ff8a56d3e78e2a49cbe5f7e4031c89a5b512
Wenlei <2856237457@qq.com>
2020-10-14 21:10:33 +0800

    save before ejecting

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,63 @@
+import React,{Component} from "react";
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+
+import './login.less'
+export default class Login extends Component{
+
+  onFinish = () => {
+    console.log('登录成功！')
+  };
+
+  render() {
+    return (
+      <div className={'login'}>
+        <header className={'login-header'}>
+          <h1>乐程官网</h1>
+        </header>
+        <section className={'login-content'}>
+          <h2>用户登录</h2>
+          <Form
+            name="normal_login"
+            className="login-form"
+            initialValues={{remember: true}}
+            onFinish={this.onFinish}
+          >
+            <Form.Item
+              name="username"
+              rules={[
+                {required: true, whitespace: true, message: '请输入用户名'},
+                {min: 4, message: '用户名至少要有4位'},
+                {max: 12, message: '用户名最多不超过12位'},
+                {pattern: /^[a-zA-Z0-9_]+$/, message: '用户名必须是英文、数字或下划线组成'}
+              ]}
+            >
+              <Input prefix={<UserOutlined className="site-form-item-icon"/>} placeholder="Username"/>
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                {required: true, whitespace: true, message: '请输入密码'},
+                {min: 4, message: '密码至少要有4位'},
+                {max: 12, message: '密码最多不超过12位'},
+                {pattern: /^[a-zA-Z0-9.]+$/, message: '密码必须是英文、数字或.组成'}
+              ]}
+            >
+              <Input
+                prefix={<LockOutlined className="site-form-item-icon"/>}
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" className="login-form-button">
+                登录
+              </Button>
+            </Form.Item>
+          </Form>
+        </section>
+      </div>
+    )
+  }
+
+}
